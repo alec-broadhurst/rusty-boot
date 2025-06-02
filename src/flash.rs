28d02495a3cdf11a6d77d9c42822cd5f0f732812
alec-broadhurst <alecrbroad@gmail.com>
2025-06-01 22:57:32 -0500

    Add spm_poll calls and reenable_rww function to Flash

--- src/flash.rs
+++ src/flash.rs
@@ -3,13 +3,23 @@ pub struct Flash;
 impl Flash {
     pub unsafe fn write_page(page_address: u16, buffer_ptr: *const u8) {
         extern "C" {
+            fn spm_poll();
             fn erase_page(page_address: u16);
             fn fill_page_buffer(word_dest: u16, buffer_ptr: *const u8);
             fn write_page(page_address: u16);
         }
 
         erase_page(page_address);
+        spm_poll();
         fill_page_buffer(page_address, buffer_ptr);
+        spm_poll();
         write_page(page_address);
+    }
+
+    pub unsafe fn reenable_rww() {
+        extern "C" {
+            fn reenable_rww();
+        }
+        reenable_rww();
     }
 }
