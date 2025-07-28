888d28277ad5915330c8f3f8a0eb0099f91e129c
alec <alecrbroad@gmail.com>
2025-07-27 20:23:50 -0500

    added and moved spm_poll call into assembly funcitons

--- src/flash.rs
+++ src/flash.rs
@@ -1,16 +1,13 @@
 /// Writes a 128-byte page to flash memory at the given word address.
 pub unsafe fn write_page(page_address: u16, buffer_ptr: *const u8) {
     extern "C" {
-        fn spm_poll();
         fn erase_page(page_address: u16);
         fn fill_page_buffer(word_dest: u16, buffer_ptr: *const u8);
         fn write_page(page_address: u16);
     }
 
     erase_page(page_address);
-    spm_poll();
     fill_page_buffer(page_address, buffer_ptr);
-    spm_poll();
     write_page(page_address);
 }
 
