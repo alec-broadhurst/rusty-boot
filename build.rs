8caa6bf89df07b0ad578a8975fad6e13e3861a2c
alec <alecrbroad@gmail.com>
2025-12-14 15:56:48 -0600

    updated build script

--- build.rs
+++ build.rs
@@ -1,41 +1,28 @@
-use std::env;
-use std::path::PathBuf;
-use std::process::Command;
+use std::path::Path;
 
 fn main() {
-    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let mcu = "-mmcu=atmega328p";
+    let mut build = cc::Build::new();
 
-    for src in &[
-        "src/asm/start.S",
-        "src/asm/erase_page.S",
-        "src/asm/write_page.S",
-        "src/asm/spm_poll.S",
-        "src/asm/fill_page_buffer.S",
-        "src/asm/reenable_rww.S",
-        "src/asm/jmp_to_app.S",
-    ] {
-        let obj = out.join(src.split('/').last().unwrap().replace(".S", ".o"));
-        Command::new("avr-gcc")
-            .args(&[mcu, "-c", src, "-o"])
-            .arg(&obj)
-            .status()
-            .expect("Compile failed");
-    }
+    let asm_dir = Path::new("src/asm");
+    println!("cargo:rerun-if-changed={}", asm_dir.display());
 
-    let mut ar = Command::new("avr-ar");
-    let lib = out.join("libasm.a");
-    ar.arg("crs").arg(&lib);
-    for file in out.read_dir().unwrap() {
-        let path = file.unwrap().path();
-        if path.extension().and_then(|e| e.to_str()) == Some("o") {
-            ar.arg(path);
+    if asm_dir.exists() && asm_dir.is_dir() {
+        for entry in asm_dir.read_dir().expect("Failed to read asm directory") {
+            let entry = entry.expect("Failed to read directory entry");
+            let path = entry.path();
+
+            if let Some(ext) = path.extension() {
+                if ext == "s" {
+                    println!("cargo:rerun-if-changed={}", path.display());
+                    build.file(path);
+                }
+            }
         }
     }
-    ar.status().expect("Archive failed");
 
-    println!("cargo:rustc-link-search=native={}", out.display());
-    println!("cargo:rustc-link-lib=static=asm");
-    println!("cargo:rustc-link-lib=gcc");
-    println!("cargo:rustc-link-lib=c");
+    build
+        .compiler("avr-gcc")
+        .archiver("avr-ar")
+        .flag("-mmcu=atmega328p")
+        .compile("asm");
 }
