52cd401000b71b678e79c32bad4da22698760b12
alec <alecrbroad@gmail.com>
2026-01-08 16:55:27 -0600

    some communication issues

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
+#![feature(asm_experimental_arch)]
+
+use core::arch::asm;
+use core::panic::PanicInfo;
 
 mod flash;
 mod serial;
 mod timer;
-
-use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -13,59 +15,172 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
-pub extern "C" fn main() -> ! {
-    timer::init();
-    serial::init();
-
-    extern "C" {
-        fn jmp_to_app();
-    }
-
-    let mut has_data = false;
-
-    for _ in 0..500 {
-        if serial::data_available() {
-            has_data = true;
-            break;
-        };
-
-        timer::wait_ms(1);
-    }
-
-    if !has_data {
-        unsafe {
-            flash::reenable_rww();
-            jmp_to_app();
-        }
-
-        loop {}
-    }
-
-    let mut page_address: u16 = 0x0000;
-    let mut page_buffer: [u8; 128] = [0; 128];
-
-    for _ in (0..0x7800).step_by(128) {
-        for i in 0..128 {
-            match serial::read_byte(1000) {
-                Some(byte) => page_buffer[i] = byte,
-                None => unsafe {
-                    flash::reenable_rww();
-                    jmp_to_app();
-                },
-            };
-        }
-
-        unsafe {
-            flash::write_page(page_address, page_buffer.as_ptr());
-        }
-
-        page_address += 64;
-    }
-
-    unsafe {
-        flash::reenable_rww();
-        jmp_to_app();
-    }
-
+pub extern "C" fn abort() -> ! {
     loop {}
+}
+
+// === STK500v1 command and response values ===
+const RESP_STK_OK: u8 = 0x10;
+const RESP_STK_INSYNC: u8 = 0x14;
+const EOP: u8 = 0x20;
+const CMD_GET_SYNC: u8 = 0x30;
+const CMD_GET_PARAMETER: u8 = 0x41;
+const CMD_LEAVE_PROGMODE: u8 = 0x51;
+const CMD_LOAD_ADDR: u8 = 0x55;
+const CMD_STK_UNIVERSAL: u8 = 0x56;
+const CMD_PROG_PAGE: u8 = 0x64;
+const CMD_STK_READ_PAGE: u8 = 0x74;
+const CMD_READ_SIGNATURE: u8 = 0x75;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    serial::init(19200);
+
+    let mut cur_addr: u16 = 0x0000;
+    let mut page_base: u16 = 0x0000;
+
+    loop {
+        let cmd_byte: u8 = serial::read_byte();
+
+        match cmd_byte {
+            CMD_GET_SYNC => {
+                let _eop: u8 = serial::read_byte();
+
+                serial::send_byte(RESP_STK_INSYNC);
+                serial::send_byte(RESP_STK_OK);
+            }
+
+            CMD_LOAD_ADDR => {
+                let addr_lo: u8 = serial::read_byte();
+                let addr_hi: u8 = serial::read_byte();
+
+                cur_addr = ((addr_hi as u16) << 8) | addr_lo as u16;
+                page_base = cur_addr & !0x3F;
+
+                let _eop: u8 = serial::read_byte();
+
+                serial::send_byte(RESP_STK_INSYNC);
+                serial::send_byte(RESP_STK_OK);
+            }
+
+            CMD_PROG_PAGE => {
+                let len_hi: u8 = serial::read_byte();
+                let len_lo: u8 = serial::read_byte();
+                let memtype: u8 = serial::read_byte();
+
+                let len: u16 = (len_hi as u16) << 8 | len_lo as u16;
+
+                for i in 0..(len >> 1) {
+                    let word_lo: u8 = serial::read_byte();
+                    let word_hi: u8 = serial::read_byte();
+                    let word: u16 = ((word_hi as u16) << 8) | word_lo as u16;
+
+                    if memtype == b'F' {
+                        flash::word_to_buf(word, cur_addr + i);
+                    }
+                }
+
+                let _eop: u8 = serial::read_byte();
+
+                serial::send_byte(RESP_STK_INSYNC);
+
+                flash::program_page(page_base);
+                flash::reenable_rww();
+
+                serial::send_byte(RESP_STK_OK);
+            }
+
+            CMD_STK_READ_PAGE => {
+                let len_hi: u8 = serial::read_byte();
+                let len_lo: u8 = serial::read_byte();
+                let memtype: u8 = serial::read_byte();
+                let _eop: u8 = serial::read_byte();
+
+                serial::send_byte(RESP_STK_INSYNC);
+
+                let len: u16 = ((len_hi as u16) << 8) | (len_lo as u16);
+                let mut byte_addr: u16 = cur_addr << 1;
+
+                if memtype == b'F' {
+                    let mut z: u16 = byte_addr;
+
+                    for _ in 0..len {
+                        let byte: u8;
+
+                        unsafe {
+                            asm!(
+                                "lpm {b}, Z+",
+                                b = out(reg) byte,
+                                inout("Z") z,
+                                options(nostack)
+                            );
+                        }
+
+                        serial::send_byte(byte);
+                        byte_addr += 1;
+                    }
+
+                    cur_addr = byte_addr >> 1;
+                }
+
+                serial::send_byte(RESP_STK_OK);
+            }
+
+            CMD_READ_SIGNATURE => {
+                let _eop = serial::read_byte();
+
+                serial::send_byte(RESP_STK_INSYNC);
+
+                /* atmega328p signature */
+                serial::send_byte(0x1E);
+                serial::send_byte(0x95);
+                serial::send_byte(0x0F);
+
+                serial::send_byte(RESP_STK_OK);
+            }
+
+            CMD_STK_UNIVERSAL => {
+                serial::read_byte();
+                serial::read_byte();
+                serial::read_byte();
+                serial::read_byte();
+                let _eop: u8 = serial::read_byte();
+
+                serial::send_byte(RESP_STK_INSYNC);
+                serial::send_byte(0x03);
+                serial::send_byte(RESP_STK_OK);
+            }
+
+            CMD_GET_PARAMETER => {
+                let _parameter: u8 = serial::read_byte();
+                let _eop: u8 = serial::read_byte();
+
+                serial::send_byte(RESP_STK_INSYNC);
+                serial::send_byte(0x03);
+                serial::send_byte(RESP_STK_OK);
+            }
+
+            CMD_LEAVE_PROGMODE => {
+                let _eop = serial::read_byte();
+
+                serial::send_byte(RESP_STK_INSYNC);
+                serial::send_byte(RESP_STK_OK);
+
+                unsafe {
+                    asm!("rjmp 0x0000");
+                }
+            }
+
+            _ => {
+                let mut byte: u8 = serial::read_byte();
+
+                while byte != EOP {
+                    byte = serial::read_byte();
+                }
+
+                serial::send_byte(RESP_STK_INSYNC);
+                serial::send_byte(RESP_STK_OK);
+            }
+        }
+    }
 }
