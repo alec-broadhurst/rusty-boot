61814f2e3d70d6625bf9033f698993620cececb8
alec <alecrbroad@gmail.com>
2026-02-10 14:55:08 -0600

    create serial struct

--- src/serial.rs
+++ src/serial.rs
@@ -11,62 +11,50 @@ const UBRR0L: *mut u8 = 0xC4 as *mut u8;
 const UBRR0H: *mut u8 = 0xC5 as *mut u8;
 const UDR0: *mut u8 = 0xC6 as *mut u8;
 
-// === System Register Address ===
-const SREG: *mut u8 = 0x5F as *mut u8;
-
 // === Bit positions ===
 // UCSR0A
 const U2X0: u8 = 1;
 const UDRE0: u8 = 5;
-const TXC0: u8 = 6;
 const RXC0: u8 = 7;
 
 // UCSR0B
-const UCSZ02: u8 = 2;
 const TXEN0: u8 = 3;
 const RXEN0: u8 = 4;
-const UDRIE0: u8 = 5;
-const RXCIE0: u8 = 7;
 
 // UCSR0C
-const UCPOL0: u8 = 0;
 const UCSZ00: u8 = 1;
 const UCSZ01: u8 = 2;
-const USBS0: u8 = 3;
-const UPM00: u8 = 4;
-const UPM01: u8 = 5;
-const UMSEL00: u8 = 6;
-const UMSEL01: u8 = 7;
 
-pub fn init(baud: u32) {
-    unsafe {
+pub struct Serial;
+
+impl Serial {
+    pub fn new(baud: u32) -> Self {
         let ubrr: u16 = ((F_CPU / (8u32 * baud)) - 1) as u16;
-        write_volatile(UCSR0A, 1 << U2X0);
 
-        // Set baud rate
-        write_volatile(UBRR0H, (ubrr >> 8) as u8);
-        write_volatile(UBRR0L, ubrr as u8);
+        unsafe {
+            write_volatile(UCSR0A, 1 << U2X0);
+            write_volatile(UBRR0H, (ubrr >> 8) as u8);
+            write_volatile(UBRR0L, ubrr as u8);
+            write_volatile(UCSR0C, (1 << UCSZ00) | (1 << UCSZ01));
+            write_volatile(UCSR0B, (1 << TXEN0) | (1 << RXEN0));
+        }
 
-        // Set 8N1: 8-bit data, no parity, 1 stop bit
-        write_volatile(UCSR0C, (1 << 1) | (1 << 2));
-
-        // Enable TX/RX
-        write_volatile(UCSR0B, (1 << TXEN0) | (1 << RXEN0));
+        Serial
     }
-}
 
-#[inline(never)]
-pub fn read_byte() -> u8 {
-    unsafe {
-        while (read_volatile(UCSR0A) & (1 << RXC0)) == 0 {}
-        read_volatile(UDR0)
+    #[inline(never)]
+    pub fn read_byte(&mut self) -> u8 {
+        unsafe {
+            while (read_volatile(UCSR0A) & (1 << RXC0)) == 0 {}
+            read_volatile(UDR0)
+        }
     }
-}
 
-#[inline(never)]
-pub fn send_byte(data: u8) {
-    unsafe {
-        while (read_volatile(UCSR0A) & (1 << UDRE0)) == 0 {}
-        write_volatile(UDR0, data);
+    #[inline(never)]
+    pub fn send_byte(&mut self, data: u8) {
+        unsafe {
+            while (read_volatile(UCSR0A) & (1 << UDRE0)) == 0 {}
+            write_volatile(UDR0, data);
+        }
     }
 }
